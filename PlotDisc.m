62781a4b179597879eb689f5c0b9ac71019e16a1
Sandra J. Poulson <sandrapoulson@fastmail.com>
2021-11-03 19:23:27 -0400

    Update PlotDisc.m

--- PlotDisc.m
+++ PlotDisc.m
@@ -1,10 +1,6 @@
 function PlotDisc(radius, xSize, ySize, xCenter, yCenter)
 
-
-[columnsInImage, rowsInImage] = meshgrid(1:xSize, 1:ySize);
-discPixels = (rowsInImage - yCenter).^2 + (columnsInImage - xCenter).^2 <= radius.^2;
-
-image(discPixels);
-colormap([1 1 1; 0 0 0]);
+set(gcf, 'position', [0, 0, xSize, ySize])
+plot(xCenter, yCenter, '.k', 'MarkerSize', radius)
 
 end
